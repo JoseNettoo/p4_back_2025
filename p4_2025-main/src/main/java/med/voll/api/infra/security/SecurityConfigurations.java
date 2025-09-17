21863dcce50113aae1e2794c1cb0bd7f5e908db1
José Neto <josepereiraneto1995@gmail.com>
2025-09-16 21:15:27 -0300

    adicionando login e senha

--- p4_2025-main/src/main/java/med/voll/api/infra/security/SecurityConfigurations.java
+++ p4_2025-main/src/main/java/med/voll/api/infra/security/SecurityConfigurations.java
@@ -2,10 +2,14 @@ package med.voll.api.infra.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -20,5 +24,13 @@ public class SecurityConfigurations {
                         (SessionCreationPolicy.STATELESS))
                 .build();
     }
+    @Bean
+    public AuthenticationManager authenticationManager (AuthenticationConfiguration configuration)throws Exception{
+        return configuration.getAuthenticationManager();
 
+    }
+
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
 }
