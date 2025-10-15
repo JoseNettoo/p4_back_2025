562ce5e3c4f523953af85faf0419437875db8b60
José Neto <josepereiraneto1995@gmail.com>
2025-10-14 21:13:38 -0300

    Atualizando 14-10

--- p4_2025-main/src/main/java/med/voll/api/infra/security/SecurityFilter.java
+++ p4_2025-main/src/main/java/med/voll/api/infra/security/SecurityFilter.java
@@ -5,6 +5,10 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import med.voll.api.domain.usuario.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -12,20 +16,37 @@ import java.io.IOException;
 @Component
 public class SecurityFilter extends OncePerRequestFilter {
 
+    @Autowired
+    private TokenService tokenService;
+
+    @Autowired
+    private UsuarioRepository repository;
+
+
+
     @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
         var tokenJWT = recuperarToken(request);
-        System.out.println("Token jwt: " +tokenJWT);
+        if (tokenJWT != null){
+            var subject = tokenService.getSubject(tokenJWT);
+            var usuario = repository.findByLogin(subject);
+
+            var authentication = new UsernamePasswordAuthenticationToken(
+                    usuario,null,usuario.getAuthorities()
+            );
+            SecurityContextHolder.getContext()
+                    .setAuthentication(authentication);
+        }
         filterChain.doFilter(request,response);
     }
 
     private String recuperarToken(HttpServletRequest request) {
         var authorizationHeader =
                 request.getHeader("Authorization");
-        if(authorizationHeader == null){
-            throw new RuntimeException("Token JWT não enivado" +
-                    "no cabeçalho Authorization");
+        if(authorizationHeader != null){
+            return authorizationHeader.replace("Bearer ","");
         }
-        return authorizationHeader;
+        return null;
     }
 }
